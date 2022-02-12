6a6d18e0b4295d31632d7c9c3012543580895942
Pedro <lobophf@gmail.com>
2022-02-12 16:19:06 -0300

    first commit

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,17 @@
+package com.api.parkingcontrol.repositories;
+
+import java.util.UUID;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
+
+    public boolean existsByLicensePlateCar(String licensePlateCar);
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    public boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
